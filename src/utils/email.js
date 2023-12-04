c5d223e1f3fa1f1610c26dfe76026dbcbc6e53b2
branxrams <braraos21@gmail.com>
2023-12-04 14:55:48 -0500

    Fix: email link fixed

--- src/utils/email.js
+++ src/utils/email.js
@@ -18,7 +18,7 @@ export default async function emailRegistro(userData) {
         text: "Comprueba tu cuenta en dayanaSport",
         html: `<p>Hola: ${nombre} Comprueba tu cuenta en DayanaSport</p>
         <p>Tu cuenta ya casi esta lista, solo dale click en el siguiente enlace</p>
-        <a href="${process.env.FRONTEND_URL}/usuario/${token}" target="_blank">Comprobar Cuenta</a>
+        <a href="https://${process.env.FRONTEND_URL}/usuario/${token}" target="_blank">Comprobar Cuenta</a>
         <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
         `,
     });
